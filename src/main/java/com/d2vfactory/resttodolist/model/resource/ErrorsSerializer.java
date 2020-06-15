b99cd0ae7885e96c1d8bfee65562e980e7c90410
Gilju Dev Factory <d2vfactory@gmail.com>
2020-06-16 01:22:51 +0900

    Errors에 대한 Serializer 처리

---
+++ src/main/java/com/d2vfactory/resttodolist/model/resource/ErrorsSerializer.java
@@ -0,0 +1,36 @@
+package com.d2vfactory.resttodolist.model.resource;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.validation.Errors;
+
+import java.io.IOException;
+import java.util.Objects;
+
+@JsonComponent
+public class ErrorsSerializer extends JsonSerializer<Errors> {
+    @Override
+    public void serialize(Errors errors, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        gen.writeStartArray();
+        errors.getFieldErrors().forEach(e -> {
+            try {
+                gen.writeStartObject();
+                gen.writeStringField("field", e.getField());
+                gen.writeStringField("objectName", e.getObjectName());
+                gen.writeStringField("code", e.getCode());
+                gen.writeStringField("defaultMessage", e.getDefaultMessage());
+                Object rejectedValue = e.getRejectedValue();
+                if (Objects.nonNull(rejectedValue)) {
+                    gen.writeStringField("rejectedValue", rejectedValue.toString());
+                }
+                gen.writeEndObject();
+            } catch (Exception error) {
+                error.printStackTrace();
+            }
+        });
+
+        gen.writeEndArray();
+    }
+}
