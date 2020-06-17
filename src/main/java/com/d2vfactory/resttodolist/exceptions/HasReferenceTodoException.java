569b8a48318ca51f578f4af96d1187ba843f54db
Gilju Dev Factory <d2vfactory@gmail.com>
2020-06-18 08:44:13 +0900

    예외 메시지 수정 : 참조된 -> 참조한

--- src/main/java/com/d2vfactory/resttodolist/exceptions/HasReferenceTodoException.java
+++ src/main/java/com/d2vfactory/resttodolist/exceptions/HasReferenceTodoException.java
@@ -3,7 +3,7 @@ package com.d2vfactory.resttodolist.exceptions;
 public class HasReferenceTodoException extends RuntimeTodoException {
 
     public HasReferenceTodoException() {
-        super("참조된 할일 목록이 존재합니다.");
+        super("참조한 할일 목록이 존재합니다.");
     }
 
 }
